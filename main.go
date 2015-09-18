f40008e93047d26b8a109e69f4875ca13d9fbb44
Bryan Moyles <bryan@teltech.co>
2015-09-18 19:14:21 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/microplatform-io/platform"
+)
+
+func main() {
+	service, err := platform.NewBasicService()
+	if err != nil {
+		log.Fatalf("failed to create service: %s", err)
+	}
+
+	service.AddHandler("/platform/create/echo", platform.HandlerFunc(func(responseSender platform.ResponseSender, request *platform.Request) {
+		responseSender.Send(platform.GenerateResponse(request, &platform.Request{
+			Routing: &platform.Routing{
+				RouteTo: []*platform.Route{
+					&platform.Route{Uri: platform.String("resource:///platform/reply/echo")},
+				},
+			},
+			Context:   request.Context,
+			Payload:   request.Payload,
+			Completed: platform.Bool(true),
+		}))
+	}), 1)
+
+	service.Run()
+}

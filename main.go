108fb3c2c3364839ac352a2278069a6ab42079fc
ronniekritou <ronniekritou@gmail.com>
2015-09-21 10:43:07 -0400

    added documentation handler

--- main.go
+++ main.go
@@ -21,5 +21,28 @@ func main() {
 		}))
 	}), 1)
 
+	service.AddHandler("/platform/get/documentation", platform.HandlerFunc(func(responseSender platform.ResponseSender, request *platform.Request) {
+		responseSender.Send(platform.GenerateResponse(request, &platform.Request{
+			Routing: platform.RouteToUri("resource:///platform/reply/documentation"),
+			Payload: GetProtoBytes(&platform.Documentation{
+				ServiceRoutes: []*platform.ServiceRoute{
+					&platform.ServiceRoute{
+						Description: platform.String("Create Echo "),
+						Request:     &platform.Route{Uri: platform.String("microservice:///platform/create/echo")},
+						Responses: []*platform.Route{
+							&platform.Route{Uri: platform.String("resource:///platform/reply/echo")},
+						},
+						Version: platform.String("1.0"),
+					},
+				},
+			}),
+		}))
+	}), 1)
+
 	service.Run()
+}
+
+func GetProtoBytes(message platform.Message) []byte {
+	protoBytes, _ := platform.Marshal(message)
+	return protoBytes
 }
